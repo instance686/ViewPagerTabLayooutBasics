5f34b9bb881bd35600495f2f3729aba4a6071a09
ayush <ayush.cir@gmail.com>
2017-11-18 18:10:46 +0530

    ListView and Adapters added

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -13,6 +15,7 @@ public class NumbersActivity extends AppCompatActivity {
 
     List<String> words=new ArrayList<>();
     TextView wordView;
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,24 +115,15 @@ public class NumbersActivity extends AppCompatActivity {
 
 
         LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-    for(String word:words){
-        wordView=new TextView(this);
-        wordView.setText(word);
-        rootView.addView(wordView);
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
+        ArrayAdapter<String> items=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,words);
+//    for(String word:words){
+//        wordView=new TextView(this);
+//        wordView.setText(word);
+//        rootView.addView(wordView);
+//    }
+        list=new ListView(this);
+        list.setAdapter(items);
+        rootView.addView(list);
 
     }
 }
